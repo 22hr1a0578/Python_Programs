058c38fa6bb9b1e09c8db9cf4836e57a0bb60904
N.latha <154883112+22hr1a0578@users.noreply.github.com>
2026-03-31 22:15:31 +0530

    Create kwarg.py

---
+++ kwarg.py
@@ -0,0 +1,9 @@
+ def kwlen(name,**values): #keyword variable length argument
+...     print("name =%s"%name)
+...     print(values)
+... kwlen("latha",name1="Latha",age=22,salary=22000)
+...
+
+#Output
+name =latha
+{'name1': 'Latha', 'age': 22, 'salary': 22000}
