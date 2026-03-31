5740d097da94860f033c1691e96d52f604ec9db7
N.latha <154883112+22hr1a0578@users.noreply.github.com>
2026-03-31 22:25:04 +0530

    Update kwarg.py

--- kwarg.py
+++ kwarg.py
@@ -7,3 +7,46 @@
 #Output
 name =latha
 {'name1': 'Latha', 'age': 22, 'salary': 22000}
+
+#fetch items from kwarg
+ def kwarg(**b):
+...     print(type(b))
+...     for c,d in b.items():
+...         print("Keys ={}".format(c))
+...         print("values={}".format(d))
+... kwarg(name="Latha",age=22,salary=22000)
+...
+
+#output
+<class 'dict'>
+Keys =name
+values=Latha
+Keys =age
+values=22
+Keys =salary
+values=22000
+
+
+#fetching only keys
+def kwarg(**b):
+...     for c in b.keys():
+...         print("keys={}".format(c))
+... kwarg(name="Latha",age=22,salary=22000)
+...
+
+#output
+keys=name
+keys=age
+keys=salary
+
+#fetching values
+def kwarg(**b):
+...     for c in b.values():
+...         print("values={}".format(c))
+... kwarg(name="Latha",age=22,salary=22000)
+...
+
+#output
+values=Latha
+values=22
+values=22000
